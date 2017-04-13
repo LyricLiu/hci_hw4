04d35eb93d337a4c433ce7fcbb61585730fc0c1a
LyricLiu <yichiliu@foxmail.com>
2017-04-13 10:39:06 -0400

    add click event

---
+++ script/main.js
@@ -0,0 +1,81 @@
+(function() {
+ $("#navi1").click(function(){
+ 	window.location.href='#overview';
+ })
+
+ $("#navi2").click(function(){
+ 	window.location.href='#survey';
+ })
+
+ $("#navi3").click(function(){
+ 	window.location.href='#task';
+ })
+
+ $("#navi4").click(function(){
+ 	window.location.href='#lowfi1';
+ })
+
+ $("#navi5").click(function(){
+ 	window.location.href='#feedback';
+ })
+
+ $("#navi6").click(function(){
+ 	window.location.href='#highfi';
+ })
+
+  $("#navi7").click(function(){
+ 	window.location.href='#evaluation';
+ })
+
+ $("#navi1_1").click(function(){
+ 	window.location.href='#motivation';
+ })
+
+ $("#navi1_2").click(function(){
+ 	window.location.href='#user';
+ })
+
+ $("#navi2_1").click(function(){
+ 	window.location.href='#observation';
+ })
+
+ $("#navi2_2").click(function(){
+ 	window.location.href='#affinity';
+ })
+
+ $("#navi3_1").click(function(){
+ 	window.location.href='#persona';
+ })
+
+ $("#navi3_2").click(function(){
+ 	window.location.href='#implications';
+ })
+
+ $("#navi4_1").click(function(){
+ 	window.location.href='#lowfi2';
+ })
+
+ $("#navi4_2").click(function(){
+ 	window.location.href='#lowfi3';
+ })
+
+ $("#navi5_1").click(function(){
+ 	window.location.href='#feedback';
+ })
+
+ $("#navi5_2").click(function(){
+ 	window.location.href='#feedback2';
+ })
+
+ $("#navi6_1").click(function(){
+ 	window.location.href='#hifi_p1';
+ })
+
+ $("#navi6_2").click(function(){
+ 	window.location.href='#hifi_p';
+ })
+
+
+
+
+})();
